16995be836fb829c836e0284c15a5f9393eb63e4
sadrisso <sadriisso304@gmail.com>
2025-08-03 06:47:54 +0600

    created a counter slice for increment and decrement counter state

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { counterSlice } from "../features/counter/counterSlice";
+import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
   reducer: {
-   
-  }
-})
+    counter: counterSlice,
+  },
+});
